3ead008eaca7436592750f7f21d3f1aca28ffbba
Joel Lujan <joelandreslujan@gmail.com>
2022-04-19 21:11:07 -0300

    Mi primer commit de git

---
+++ Leccion 1/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,18 @@
+
+
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde Java");
+        int myVariable = 25;
+//        String palabra = "/t hola";
+        System.out.println(myVariable);
+        myVariable = 30;
+        System.out.println(myVariable);
+        //tipo String
+        String miVariableCadena = "Bienvenidos";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "Sigamos creciendo en programación";
+        System.out.println(miVariableCadena);
+    }
+}
